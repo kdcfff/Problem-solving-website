9067e664dee4210fe9acae6cb76e6aac85d3f70b
kfc <2480155857@qq.com>
2025-07-01 22:24:51 +0800

    feat(question): 添加题库相关功能
    - 新增题库相关模型和 DTO
    - 实现题库的增删改查接口
    - 添加题库题目链接相关功能- 更新代码生成器的配置

--- src/main/java/com/mishicoder/service/QuestionBankService.java
+++ src/main/java/com/mishicoder/service/QuestionBankService.java
@@ -1,13 +1,53 @@
 package com.mishicoder.service;
 
-import com.mishicoder.model.entity.QuestionBank;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.mishicoder.model.dto.questionBank.QuestionBankQueryRequest;
+import com.mishicoder.model.entity.QuestionBank;
+import com.mishicoder.model.vo.QuestionBankVO;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
-* @author kdc
-* @description 针对表【question_bank(题库)】的数据库操作Service
-* @createDate 2025-06-29 01:21:35
-*/
+ * 题库服务
+ *
+ * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
+ * @from <a href="https://www.code-nav.cn">编程导航学习圈</a>
+ */
 public interface QuestionBankService extends IService<QuestionBank> {
 
+    /**
+     * 校验数据
+     *
+     * @param questionBank
+     * @param add 对创建的数据进行校验
+     */
+    void validQuestionBank(QuestionBank questionBank, boolean add);
+
+    /**
+     * 获取查询条件
+     *
+     * @param questionBankQueryRequest
+     * @return
+     */
+    QueryWrapper<QuestionBank> getQueryWrapper(QuestionBankQueryRequest questionBankQueryRequest);
+    
+    /**
+     * 获取题库封装
+     *
+     * @param questionBank
+     * @param request
+     * @return
+     */
+    QuestionBankVO getQuestionBankVO(QuestionBank questionBank, HttpServletRequest request);
+
+    /**
+     * 分页获取题库封装
+     *
+     * @param questionBankPage
+     * @param request
+     * @return
+     */
+    Page<QuestionBankVO> getQuestionBankVOPage(Page<QuestionBank> questionBankPage, HttpServletRequest request);
 }
